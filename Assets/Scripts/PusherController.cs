8ee4dc465267bf660e58768e52eb1b0ab4872980
baunilhamarga <129125831+baunilhamarga@users.noreply.github.com>
2024-10-30 22:09:43 -0300

    🚀 pusher from controller to rigid body

--- Assets/Scripts/PusherController.cs
+++ Assets/Scripts/PusherController.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class PusherController : MonoBehaviour
 {
-    private CharacterController _characterController;
+    private Rigidbody _rigidbody;
     [SerializeField] private int mass = 10;
     [SerializeField] private float speed = 50;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _characterController = GetComponent<CharacterController>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,33 +19,33 @@ public class PusherController : MonoBehaviour
         float moveX = Input.GetAxis("Vertical");
         float moveZ = Input.GetAxis("Horizontal");
 
-        Vector3 speedVector = new Vector3(-1 * moveX * speed, 0,moveZ * speed);
-        _characterController.SimpleMove(speedVector);
+        Vector3 speedVector = new Vector3(-1 * moveX * speed, 0, moveZ * speed);
+        _rigidbody.linearVelocity = speedVector;
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    private void OnCollisionEnter(Collision collision)
     {
-        Rigidbody body = hit.collider.attachedRigidbody;
-        
+        Rigidbody body = collision.collider.attachedRigidbody;
+
         if (body == null || body.isKinematic)
             return;
 
-        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.y);
-        Vector3 pusherVelocity = _characterController.velocity;
+        Vector3 pushDir = new Vector3(collision.relativeVelocity.x, 0, collision.relativeVelocity.z);
+        Vector3 pusherVelocity = _rigidbody.linearVelocity;
         Vector3 collidedVelocity = body.linearVelocity;
         float collidedMass = body.mass;
         float totalMass = collidedMass + mass;
 
-        Vector3 newPusherVelocity = 
+        Vector3 newPusherVelocity =
             ((mass - collidedMass) / totalMass) * pusherVelocity
             + ((2 * collidedMass) / totalMass) * collidedVelocity;
-        Vector3 newCollidedVelocity = 
+        Vector3 newCollidedVelocity =
             ((2 * mass) / totalMass) * pusherVelocity
             + ((collidedMass - mass) / totalMass) * collidedVelocity;
         newPusherVelocity.y = 0;
         newCollidedVelocity.y = 0;
 
-        // _characterController.SimpleMove(newPusherVelocity);
-        body.AddForce(newCollidedVelocity - collidedVelocity, ForceMode.VelocityChange);
+        _rigidbody.linearVelocity = newPusherVelocity;
+        body.linearVelocity = newCollidedVelocity;
     }
 }
